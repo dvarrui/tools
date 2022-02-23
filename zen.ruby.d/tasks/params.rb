ec002d1d7babb2c9deda771ac370c7b922b36c8d
David Vargas <dvarrui@protonmail.com>
2022-02-23 13:27:34 +0000

    [U] .

--- zen.ruby.d/tasks/params.rb
+++ zen.ruby.d/tasks/params.rb
@@ -1,7 +1,7 @@
 
 module Params
   NAME     = 'zenruby'
-  VERSION  = '0.0.3'
+  VERSION  = '0.0.5'
   GEMNAME  = 'zen-ruby'
   HOMEPAGE = 'https://github.com/dvarrui/tools/tree/main/zen.ruby.d'
 end
