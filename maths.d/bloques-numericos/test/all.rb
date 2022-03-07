bd1bfa060b13edb316d125a8b1174386b1cb580e
David Vargas <dvarrui@protonmail.com>
2022-03-07 22:45:29 +0000

    [N] .

---
+++ maths.d/bloques-numericos/test/all.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+
+require_relative 'paso_01_test'
+#require_relative 'paso_01_test'
+#require_relative 'paso_01_test'
+#require_relative 'paso_01_test'
---
+++ maths.d/bloques-numericos/test/paso_01_test.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require_relative '../01/panel'
+
+class Paso01 < Minitest::Test
+
+  def setup
+    @panel1 = Panel.new(4)
+  end
+
+  def test_panel_new_4
+    panel = @panel1
+    row = Array.new(4, 0)
+
+    assert_equal row, panel.data[0]
+    assert_equal row, panel.data[1]
+    assert_equal row, panel.data[2]
+    assert_equal row, panel.data[3]
+  end
+
+  def test_panel_to_s
+    panel = @panel1
+    text = "[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]"
+    assert_equal text, panel.to_s
+  end
+end
