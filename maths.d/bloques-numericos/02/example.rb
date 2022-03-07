2dfb08b349436ad29bed4f540ab436e44bb7c02d
David Vargas <dvarrui@protonmail.com>
2022-03-07 23:04:40 +0000

    [U] .

--- maths.d/bloques-numericos/02/example.rb
+++ maths.d/bloques-numericos/02/example.rb
@@ -5,7 +5,7 @@ require_relative 'pretty'
 
 puts "[PASO 2] Rellenar la sección de 3x3 con valores del 1 al 9."
 
-panel = Panel.new(4)
+panel = Panel.new
 panel.random_fill
 
 Pretty.print panel
--- maths.d/bloques-numericos/02/panel.rb
+++ maths.d/bloques-numericos/02/panel.rb
@@ -3,10 +3,10 @@
 class Panel
   attr_reader :data
 
-  def initialize(size)
-    @size = size
+  def initialize
+    @size = 4
     @data = []
-    size.times { @data <<= Array.new(@size, 0) }
+    @size.times { @data <<= Array.new(@size, 0) }
   end
 
   def to_s
