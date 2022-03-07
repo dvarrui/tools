9ce862d59d4b8640ef9a809254a10e0ce3fc981a
David Vargas <dvarrui@protonmail.com>
2022-03-07 23:14:49 +0000

    [N] .

---
+++ maths.d/bloques-numericos/03/example.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require_relative 'panel'
+require_relative 'pretty'
+
+puts "[PASO 2] Rellenar la sección de 3x3 con valores del 1 al 9."
+
+panel = Panel.new
+panel.random_fill
+panel.calculate_sum
+
+Pretty.print panel
