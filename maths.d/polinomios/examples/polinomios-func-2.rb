64d5ab889b6d4b1b14114c80539c4c2d612a1630
David Vargas <dvarrui@protonmail.com>
2022-03-27 20:21:27 +0100

    [F] .

--- maths.d/polinomios/examples/polinomios-func-2.rb
+++ maths.d/polinomios/examples/polinomios-func-2.rb
@@ -4,14 +4,18 @@ require_relative '../lib/func.1/monomio'
 require_relative '../lib/func.1/polinomio'
 
 
-puts "[-] Polinomios a factorizar"
-poli = [ [1,3], [-1,2], [-4,0] ]
-Polinomio.puts_a_to_s poli
+puts "[-] Polinomios a factorizar x³-x²-4"
+poli = { 3 => 1, 2 => -1, 0 => -4 }
+Polinomio.puts_h_to_s poli
 
-puts "[-] Factor 1"
-fac1 = [ [1,1], [1,0]]
-Polinomio.puts_a_to_s fac1
+puts "[-] Factor 1: x-2"
+fac1 = { 1 => 1, 0 => -2 }
+Polinomio.puts_h_to_s fac1
 
-puts "[-] Factor 2"
-fac2 = [ [-4,2], [3,0] ]
-Polinomio.puts_a_to_s fac2
+puts "[-] Factor 2: x²+x+2"
+fac2 = { 2 => 1, 1 => 1, 0 => 2 }
+Polinomio.puts_h_to_s fac2
+
+puts "[-] Comprobamos que: poli = fac1 * fac2"
+mul = Polinomio.mul(fac1, fac2)
+Polinomio.puts_h_to_s mul
