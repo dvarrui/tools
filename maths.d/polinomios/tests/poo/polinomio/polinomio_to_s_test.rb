ad3d705a3d443e2cd6982c292121ff9042bb9e60
David Vargas <dvarrui@protonmail.com>
2022-03-28 21:21:29 +0100

    [U] .

--- maths.d/polinomios/tests/poo/polinomio/polinomio_to_s_test.rb
+++ maths.d/polinomios/tests/poo/polinomio/polinomio_to_s_test.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require_relative '../../lib/poo/monomio'
+require_relative '../../../lib/poo/monomio'
 
 class PooPolinomioTosTest < Test::Unit::TestCase
 
