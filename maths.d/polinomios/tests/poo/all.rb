ebd3eeb8db93a15ab5a59c90343c3cba334ca03b
David Vargas <dvarrui@protonmail.com>
2022-03-28 21:49:39 +0100

    [U] .

---
+++ maths.d/polinomios/tests/poo/all.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require_relative 'monomio_array_test'
+require_relative 'monomio_hash_test'
+require_relative 'monomio_to_s_test'
+require_relative 'monomio_to_pol_test'
+
+require_relative 'monomio_add_test'
+require_relative 'monomio_subs_test'
+require_relative 'monomio_mul_test'
+require_relative 'monomio_div_test'
+
+require_relative 'polinomio/polinomio_test'
+require_relative 'polinomio/polinomio_to_s_test'
