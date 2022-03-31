6f09042694d0dac4a48ce7bf5dc1db31f8f15f39
David Vargas <dvarrui@protonmail.com>
2022-03-31 23:22:28 +0100

    [U] .

---
+++ japones.d/game.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/hiragana'
+
+puts Hiragana.group1
+puts Hiragana.group2
--- japones.d/lib/hiragana.rb
+++ japones.d/lib/hiragana.rb
@@ -2,17 +2,23 @@
 
 # Silabario japonés
 
-hiragana = {}
-hiragana[:group1] = { a: "\u{3042}",
-                      i: "\u{3044}",
-                      u: "\u{3046}",
-                      e: "\u{3048}",
-                      o: "\u{304A}"}
+class Hiragana
 
-hiragana[:group2] = { ka: "\u{304B}",
-                      ki: "\u{304D}",
-                      ku: "\u{304F}",
-                      ke: "\u{3051}",
-                      ko: "\u{3053}"}
+  def self.group1
+    { a: "\u{3042}",
+      i: "\u{3044}",
+      u: "\u{3046}",
+      e: "\u{3048}",
+      o: "\u{304A}"
+    }
+  end
 
-puts hiragana
+  def self.group2
+    { ka: "\u{304B}",
+      ki: "\u{304D}",
+      ku: "\u{304F}",
+      ke: "\u{3051}",
+      ko: "\u{3053}"
+    }
+  end
+end
