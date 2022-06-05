acd6edc20aa0d17b48c35d8bb0491dec8d4071ac
David Vargas <dvarrui@protonmail.com>
2022-06-05 10:48:10 +0100

    [U] .

--- guess-the-os.d/ostype.rb
+++ guess-the-os.d/ostype.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby 
 
 def get_ostype
-  return 'linux' if File.exist? "/home"
-  
-  output = `env | grep OSTYPE`.chmop
+
+  begin  
+    output = `env | grep OSTYPE`.chmop
+  rescue
+    output = nil 
+  end 
+
+  return 'android' if output == 'linux-android'
   return 'linux' if output == 'linux-gnu'
-  return 'android' if output == 'linux-andriod'
- 
+
+  return 'linux' if File.exist? "/home"
   return 'windows' if File.exist? "C:\\Windows"
   return 'mac' if File.exist? "/Users"
 
